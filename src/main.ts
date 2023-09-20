f461d8c6698986005dd48e19cc2ca0626a0cb14d
Samuel VIDEAU <70749906+samuel-videau@users.noreply.github.com>
2023-09-20 12:19:44 +0700

    chore: express -> fastify (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { ApiModule } from './api.module';
 import { PORT } from './globals';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiModule, { cors: true });
+  const app = await NestFactory.create<NestFastifyApplication>(ApiModule, new FastifyAdapter());
+  app.enableCors();
   await app.listen(PORT);
 }
 bootstrap();
